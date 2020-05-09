bfd6a4ca440177559318cb0957dad481060e254b
aaaaninja <aaaaninja@aaaa.ninja>
2020-05-10 03:23:22 +0900

    はい (tutorial)

---
+++ jk.py
@@ -0,0 +1,4 @@
+from cloudia import Cloudia
+
+text = "これはCloudiaのテストです。WordCloudをつくるには本来、形態素解析の導入が必要になります。Cloudiaはmecabのような形態素解析器の導入は必要はなくnagisaを利用した動的な生成を行う事ができます。nagisaとjapanize-matplotlibは、形態素解析を必要としてきたWordCloud生成に対して、Cloudiaに対して大きく貢献しました。ここに感謝の意を述べたいと思います。"
+Cloudia(text).save('./asdf.png')
